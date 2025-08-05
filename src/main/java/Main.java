ef973fb50c4e0eb6db7f7528a0322c8c61b40781
omarr78 <omernabil2001@gmail.com>
2025-08-06 00:11:32 +0300

    clear Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int sum1 = 0,sum2 = 0;
-        int i = 0, j = n-1;
-        boolean flag = true;
-        while(j >= i){
-            if(arr[i] > arr[j]){
-                if(flag)sum1 += arr[i];
-                else sum2 += arr[i];
-                i++;
-            }
-            else{
-                if(flag)sum1 += arr[j];
-                else sum2 += arr[j];
-                j--;
-            }
-            flag = !flag;
-        }
-        System.out.println(sum1 + " " + sum2);
-    }
 
+    }
 }
