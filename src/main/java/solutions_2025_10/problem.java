5af952a8ff9e001f02d4d961b0b53594836bc322
omarr78 <omernabil2001@gmail.com>
2025-11-06 22:37:32 +0200

    problem f solved

--- src/main/java/solutions_2025_10/problem.java
+++ src/main/java/solutions_2025_10/problem.java
@@ -11,46 +11,79 @@ public class problem {
         for (int tci = 0; tci < tc; tci++) {
             int n = sc.nextInt();
             List<List<Integer>> list = new ArrayList<>();
-
-            // get max array size
-            Set<Integer> Allsizes = new TreeSet<>();
             int maxSize = 0;
             for (int i = 0; i < n; i++) {
                 int siz = sc.nextInt();
                 maxSize = Math.max(maxSize, siz);
-                Allsizes.add(siz);
                 List<Integer> addList = new ArrayList<>();
                 for (int j = 0; j < siz; j++) {
                     addList.add(sc.nextInt());
                 }
                 list.add(addList);
             }
+            list.sort(Comparator.comparingInt(List::size));
             int[] ans = new int[maxSize];
             Arrays.fill(ans, Integer.MAX_VALUE);
-            int[] sizes = Allsizes.stream().mapToInt(i -> i).toArray();
 
-            int startIdx;
-            int endIdx = 0;
-            for (int r = 0; r < sizes.length; r++) {
-                startIdx = endIdx;
-                endIdx = sizes[r];
+            Set<Integer> checkpoints = new TreeSet<>();
+            int cntSz = 0;
 
-                for (List checkList : list) {
+            for (List<Integer> arr : list) {
+                checkpoints.add(cntSz);
+                boolean isBest = false;
+                int flag = 0;
+                for (int i = 0; i < arr.size(); i++) {
+                    if(checkpoints.isEmpty()){
+                        checkpoints.add(0);
+                    }
+                    if (checkpoints.contains(i)) {
+                        if (flag == -1) {
+                            flag = 0;
+                        }
+                    }
                     // 0 means equal
                     // -1 ignore
-                    // 1 put my values
-                    int flag = 0;
-                    for (int i = startIdx; i < endIdx; i++) {
+                    // 1 put my values, or isBest = true
+                    if (isBest) {
+                        if (ans[i] == Integer.MAX_VALUE) cntSz++;
+                        else{
+                            Iterator<Integer> it = checkpoints.iterator();
+                            while (it.hasNext()) {
+                                if (it.next() > i){
+                                    it.remove();
+                                }
+                            }
+                        }
+                        ans[i] = arr.get(i);
+                    } else {
                         if (flag == 0) {
-                            if ((int) checkList.get(i) > ans[i]) {
+                            if (arr.get(i) < ans[i]) {
+                                if (ans[i] == Integer.MAX_VALUE) cntSz++;
+                                else{
+                                    Iterator<Integer> it = checkpoints.iterator();
+                                    while (it.hasNext()) {
+                                        if (it.next() > i){
+                                            it.remove();
+                                        }
+                                    }
+                                }
+                                ans[i] = arr.get(i);
                                 flag = 1;
-                            } else if ((int) checkList.get(i) < ans[i]) {
+                                isBest = true;
+                            } else if (arr.get(i) > ans[i]) {
                                 flag = -1;
                             }
                         } else if (flag == 1) {
-                            ans[i] = (int) checkList.get(i);
-                        } else { // -1
-                            break;
+                            if (ans[i] == Integer.MAX_VALUE) cntSz++;
+                            else{
+                                Iterator<Integer> it = checkpoints.iterator();
+                                while (it.hasNext()) {
+                                    if (it.next() > i){
+                                        it.remove();
+                                    }
+                                }
+                            }
+                            ans[i] = arr.get(i);
                         }
                     }
                 }
@@ -61,5 +94,72 @@ public class problem {
             System.out.println();
         }
     }
-
 }
+
+// test cases to cover
+/*
+=== INPUT ===
+
+1
+5
+3 2 2 8
+4 2 3 6 4
+2 5 1
+2 2 3
+3 4 8 1
+
+=== ACCEPTED OUTPUT ===
+2 2 8 4
+=== WRONG OUTPUT ===
+2 2 1 4
+
+================================================
+
+=== INPUT ===
+
+1
+4
+2 5 1
+1 5
+5 7 6 3 7 9
+3 8 2 9
+
+=== ACCEPTED OUTPUT ===
+5 1 3 7 9
+=== WRONG OUTPUT ===
+5 1 9 7 9
+
+=================================================
+
+=== INPUT ===
+1
+5
+4 6 9 4 7
+1 8
+1 3
+4 6 8 8 2
+4 6 6 6 8
+
+=== ACCEPTED OUTPUT ===
+3 6 6 8
+=== WRONG OUTPUT ===
+3 8 8 2
+
+================================================
+
+=== INPUT ===
+1
+5
+5 9 7 4 9 7
+4 4 5 8 1
+2 1 5
+5 2 7 7 1 4
+3 8 9 6
+
+=== ACCEPTED OUTPUT ===
+1 5 4 9 7
+=== WRONG OUTPUT ===
+1 5 4 1 4
+
+*/
+
--- src/main/java/solutions_2025_10/problem2.java
+++ src/main/java/solutions_2025_10/problem2.java
@@ -1,60 +1,53 @@
 package solutions_2025_10;
 
-
+import java.sql.Array;
 import java.util.*;
 
+import static java.lang.Integer.min;
+import static java.util.Arrays.sort;
+
 public class problem2 {
-
-    public void sort(List<List<Integer>> list, int startIdx) {
-        list.sort((o1, o2) -> {
-            int size = Math.min(o1.size(), o2.size());
-            for (int i = startIdx; i < size; i++) {
-                int diff = o1.get(i) - o2.get(i);
-                if (diff != 0) return diff;
-            }
-            return Integer.compare(o1.size(), o2.size());
-        });
-    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        problem2 pro = new problem2();
+        Scanner scanner = new Scanner(System.in);
 
-        int tc = sc.nextInt();
-        for(int tci = 0; tci < tc; tci++) {
-            int n = sc.nextInt();
-            List<List<Integer>> list = new ArrayList<>();
-
-            // get max array size
-            int maxSize = 0;
-            for (int i = 0; i < n; i++) {
-                int siz = sc.nextInt();
-                maxSize = Math.max(maxSize, siz);
-                List<Integer> addList = new ArrayList<>();
-                for (int j = 0; j < siz; j++) {
-                    addList.add(sc.nextInt());
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[][] a = new int[n][];
+            for (int i = 0; i < n; ++i) {
+                int k = scanner.nextInt();
+                a[i] = new int[k];
+                for (int j = 0; j < k; ++j) {
+                    a[i][j] = scanner.nextInt();
                 }
-                list.add(addList);
             }
-            List<Integer> minList = new ArrayList<>();
-            pro.sort(list, 0);
-
-            while (minList.size() < maxSize) {
-                int siz = list.getFirst().size();
-                for(int i = 0 ; i < siz ; i++) {
-                    minList.add(list.getFirst().get(i));
+            ArrayList<Integer> order = new ArrayList<Integer>();
+            for (int i = 0; i < n; ++i) {
+                order.add(i);
+            }
+            int nextPos = 0;
+            while (!order.isEmpty()) {
+                int finalNextPos = nextPos;
+                order.sort((i, j) -> {
+                    int len = Integer.min(a[i].length, a[j].length);
+                    for (int k = finalNextPos; k < len; ++k) {
+                        if (a[i][k] != a[j][k]) {
+                            return a[i][k] - a[j][k];
+                        }
+                    }
+                    return a[i].length - a[j].length;
+                });
+                while (nextPos < a[order.getFirst()].length) {
+                    System.out.print(a[order.getFirst()][nextPos] + " ");
+                    nextPos++;
                 }
-                pro.sort(list, minList.size());
-            }
-            for(Integer i : minList) {
-                System.out.print(i + " ");
+                ArrayList<Integer> newOrder = new ArrayList<Integer>();
+                for (int i : order) {
+                    if (a[i].length > nextPos) newOrder.add(i);
+                }
+                order = newOrder;
             }
             System.out.println();
-
         }
-
-
-
-
     }
-
 }
